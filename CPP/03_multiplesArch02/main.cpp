77b51ee549f4e5f2d99dd732eda9f835ecd70314
SrWilbur <srwilburcoins@gmail.com>
2025-09-10 21:18:30 -0300

    Agrego  ejemplo de clase multiples archivos

---
+++ CPP/03_multiplesArch02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "producto.h"
+
+
+int main()
+{
+    std::cout << "2 x 3 = " << producto(2,3) << '\n';
+    return 0;
+}
+
+
---
+++ CPP/03_multiplesArch02/producto.cpp
@@ -0,0 +1,4 @@
+int producto (int x, int y)
+{
+    return x*y;
+}
